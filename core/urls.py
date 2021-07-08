14d0112995cd60b1c3faedaf66c191fe6abdc288
Allan <allandiamantedesouza@gmail.com>
2021-07-08 17:40:32 -0300

    tabelas com nav

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import index, contato, produto, consulta
+from .views import index, contato, produto, consulta1
 
 urlpatterns = [
     path('', index, name='index'),
     path('contato/', contato, name='contato'),
     path('produto/', produto, name='produto'),
-    path('consulta/', consulta, name='consulta'),
+    path('consulta/<int:pk>', consulta1, name='consulta1'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 
 from .forms import ContatoForm, ProdutoModelForm
 from .models import Produto
@@ -9,10 +10,12 @@ from .models import Produto
 def index(request):
     return render(request, 'index.html')
 
-def consulta(request):
+def consulta1(request, pk):
+    x = Produto.objects.all()
     context = {
-        'produtos': Produto.objects.all()
-    }
+        'produtos': x[(pk*3):((1+pk)*3)],
+        'qnt_produtos': range(0,int((len(Produto.objects.all())) /3))
+        }
     return render(request, 'consulta.html', context)
 
 
@@ -54,3 +57,7 @@ def produto(request):
         return render(request, 'produto.html', context)
     else:
         return redirect('index')
+
+def pegar_produto(query=None):
+    queryset = []
+    queries = query.spliy
